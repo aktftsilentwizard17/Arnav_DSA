18839949e7461c953fba9e897dddddf69de3ecbd
arnavk <akatepallewar@gmail.com>
2026-06-23 19:59:40 +0530

    Time: 0 ms (100%), Space: 9.1 MB (39.9%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+        unordered_map<char,int> freq;
+        int count = 0;
+
+        for(char c:text){
+            freq[c]++;
+        }
+
+        return min({freq['b'],freq['a'],freq['l']/2,freq['o']/2,freq['n']});
+    }
+};
