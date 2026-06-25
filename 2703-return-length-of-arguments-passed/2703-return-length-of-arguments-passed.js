6a2f985f46b5a29c49ca9eae6d8c4aab56b9b032
arnavk <akatepallewar@gmail.com>
2026-06-25 10:24:29 +0530

    Time: 43 ms (56.74%), Space: 53.8 MB (29.3%) - LeetHub

--- 2703-return-length-of-arguments-passed/2703-return-length-of-arguments-passed.js
+++ 2703-return-length-of-arguments-passed/2703-return-length-of-arguments-passed.js
@@ -2,9 +2,8 @@
  * @param {...(null|boolean|number|string|Array|Object)} args
  * @return {number}
  */
-var argumentsLength = function(...args) {
-    return args.length;
-};
+const argumentsLength = (...args) => args.length;
+
 
 /**
  * argumentsLength(1, 2, 3); // 3
