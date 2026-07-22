da9f16769b295dbc049e3a8203838f30732d6930
arnavk <akatepallewar@gmail.com>
2026-07-22 14:46:00 +0530

    Time: 0 ms (100%), Space: 9 MB (91.46%) - LeetHub

---
+++ 2600-k-items-with-the-maximum-sum/2600-k-items-with-the-maximum-sum.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int kItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k) {
+        if(numOnes>=k){
+            return k;
+        }
+        else if((numOnes+numZeros)>=k){
+            return numOnes;
+        }
+        return numOnes-(k-(numOnes+numZeros));
+    }
+};
