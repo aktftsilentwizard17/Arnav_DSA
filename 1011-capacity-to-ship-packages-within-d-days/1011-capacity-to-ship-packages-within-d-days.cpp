d803b7ebf3ccc82f0e75f24f1e854da8f47bafc8
arnavk <akatepallewar@gmail.com>
2026-09-19 16:50:50 +0530

    Time: 7 ms (90.96%), Space: 35.2 MB (19.94%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,35 +1,34 @@
 class Solution {
-public:
-    int canShip(vector<int>&weights,int days,int capacity){
-        int daysneeded = 1;
+private:
+    bool fun(const vector<int>& weights,int days, int capacity){
         int currweight = 0;
-        for(int weight:weights){
-            if(currweight+weight>capacity){
-                daysneeded++;
-                currweight=weight;
+        int totaldays = 1;
+        for(int w:weights){
+            if(currweight+w>capacity){
+                totaldays++;
+                currweight=w;
             }
             else{
-                currweight+=weight;
+                currweight+=w;
             }
         }
-        return daysneeded<=days;
+        return totaldays<=days;
     }
-    int shipWithinDays(vector<int>& weights, int days) {
-        int low=0;
-        int high=0;
-        for(int i:weights){
-            low=max(low,i);
-            high+=i;
-        }
-        while(low<high){
+public:
+    int shipWithinDays(vector<int>&weights,int days){
+        int low = *max_element(weights.begin(),weights.end());
+        int high = accumulate(weights.begin(),weights.end(),0);
+        int ans = high;
+        while(low<=high){
             int mid = low+(high-low)/2;
-            if(canShip(weights,days,mid)){
-                high=mid;
+            if(fun(weights,days,mid)){
+                ans=mid;
+                high=mid-1;
             }
             else{
                 low=mid+1;
             }
         }
-        return low;
+        return ans;
     }
 };
