ef1dbc22f030540002b37082ad3d6a1dca07a4cc
arnavk <akatepallewar@gmail.com>
2026-09-22 22:33:26 +0530

    Time: 0 ms (100%), Space: 8 MB (8.67%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.cpp
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     bool isUgly(int n) {
-        if (n <= 0) return false;
-        
-        for (int factor : {2, 3, 5}) {
-            while (n % factor == 0) {
-                n /= factor;
+        if(n<=0) return false;
+        if(n==1) return true;
+        int factors[] = {2,3,5};
+        for(int i:factors){
+            while(n%i==0){
+                n=n/i;
             }
         }
-        
-        return n == 1;
+        return n==1;
     }
 };
