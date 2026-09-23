c39ef774c795f73a16a9a4a17496af6294f7edb5
arnavk <akatepallewar@gmail.com>
2026-09-23 19:45:12 +0530

    Time: 23 ms (54.69%), Space: 17.6 MB (26.55%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -6,10 +6,13 @@ public:
         if (n < 4) return ans;
         sort(nums.begin(),nums.end());
 
+        //choose two directly, and next two by two pointer approach
         for(int i=0;i<n;i++){
             if(i>0&&nums[i]==nums[i-1]) continue;
-            for(int j=i+1;j<n;){
+            for(int j=i+1;j<n;j++){
+                if(j>i+1&&nums[j]==nums[j-1]) continue;
                 int p = j+1; int q = n-1;
+
                 while(p<q){
                     long long sum = (long long)nums[i] + (long long)nums[j] + (long long)nums[p] + (long long)nums[q];
                     if(sum<target) p++;
@@ -18,12 +21,10 @@ public:
                         ans.push_back({nums[i],nums[j],nums[p],nums[q]});
                         p++;
                         q--;
-                        while(p<q&&nums[p]==nums[p-1]) p++;
-                        while (p < q && nums[q] == nums[q + 1]) q--;
+                        while (p<q&&nums[p]==nums[p-1]) p++;
+                        while (p<q&&nums[q]==nums[q+1]) q--;
                     }
                 }
-                j++;
-                while(j<n&&nums[j]==nums[j-1]) j++;
             }
         }
 
